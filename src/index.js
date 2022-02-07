558920cb1798f07ff24f98b1e1ee7ac1bfd84564
Suraj Singla <suraj.singla@mckinleyrice.co>
2022-02-07 18:58:12 +0530

    added getRawTransaction(0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const config = require('./config');
 const helper = require('./utils/helper')
+const web3Utils = require('web3-utils')
 
-class Uniswap {   
+class Uniswap {
 
     async getSupportedTokens() {
-        const { response } = await helper.getRequest({ url: config.SUPPORTED_TOKENS_URL });
-    
+        const { response, error } = await helper.getRequest({ url: config.SUPPORTED_TOKENS_URL });
+        if (error)
+            throw error
+        return response;
+    }
+
+    async getRawTransaction(_walletAddress, _toContractAddress, _fromContractAddress, toQuantity, fromQuantity, slippageTolerance = 1) {
+        const toContractAddress = web3Utils.toChecksumAddress(_toContractAddress)
+        const fromContractAddress = web3Utils.toChecksumAddress(_fromContractAddress)
+        const walletAddress = web3Utils.toChecksumAddress(_walletAddress)
+        const { response, error } = await helper.rawTransaction(walletAddress, fromQuantity);
+        if (error)
+            throw error
         return response;
     }
 }
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const { AlphaRouter } = require("@uniswap/smart-order-router");
+const { Token, CurrencyAmount, TradeType, Percent } = require('@uniswap/sdk-core')
+const { ethers, BigNumber } = require('ethers')
 
 const getRequest = async ({ url }) => {
     try {
@@ -8,8 +11,67 @@ const getRequest = async ({ url }) => {
         });
         return { response: response.data };
     } catch (error) {
-        return { error: [{ name: 'server', message: 'There is some issue, Please try after some time' }] };
+        return { error: [{ name: 'server', message: `There is some issue, Please try after some time. ${error.message && error.message}`, data: error.response && error.response.data ? error.response.data : {} }] };
     }
 };
-module.exports = { getRequest };
+
+const rawTransaction = async (_walletAddress, fromQuantity, slippageTolerance = 1) => {
+
+    try {
+        const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45';
+        const MY_ADDRESS = _walletAddress;
+        const web3Provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/5583a1ce54604375b02d6246936d9d53");
+
+        const router = new AlphaRouter({ chainId: 1, provider: web3Provider });
+
+        const WETH = new Token(
+            1,
+            '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+            18,
+            'WETH',
+            'Wrapped Ether'
+        );
+
+        const USDC = new Token(
+            1,
+            '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+            6,
+            'USDC',
+            'USD//C'
+        );
+
+        const typedValueParsed = fromQuantity.toString()
+        const wethAmount = CurrencyAmount.fromRawAmount(WETH, typedValueParsed);
+
+        const route = await router.route(
+            wethAmount,
+            USDC,
+            TradeType.EXACT_IN,
+            {
+                recipient: _walletAddress,
+                slippageTolerance: new Percent(slippageTolerance, 100),
+                deadline: 100
+            }
+        );
+
+        console.log(`Quote Exact In: ${route.quote.toFixed(2)}`);
+        console.log(`Gas Adjusted Quote In: ${route.quoteGasAdjusted.toFixed(2)}`);
+        console.log(`Gas Used USD: ${route.estimatedGasUsedUSD.toFixed(6)}`);
+
+        const response = {
+            data: route.methodParameters.calldata,
+            to: V3_SWAP_ROUTER_ADDRESS,
+            value: BigNumber.from(route.methodParameters.value),
+            from: MY_ADDRESS,
+            gasPrice: BigNumber.from(route.gasPriceWei),
+        };
+
+        return { response };
+    } catch (error) {
+        return { error }
+    }
+
+}
+
+module.exports = { getRequest, rawTransaction };
 
